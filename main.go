2df065b4094541bf839769032a2496401d4a9ad8
Fish-pro <zechun.chen@daocloud.io>
2023-02-03 16:01:10 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+
+	"k8s.io/component-base/cli"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+)
+
+func main() {
+	command := app.NewSchedulerCommand()
+	code := cli.Run(command)
+	os.Exit(code)
+}
